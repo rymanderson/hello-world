7869e3f0e0c4b151a096167c331509d3f1da4d7f
rymanderson <rymanderson@gmail.com>
2017-08-02 10:02:06 -0600

    %10:02

---
+++ final.m
@@ -0,0 +1,12 @@
+%Filename: Hello, World! (from MatLab)
+%Author: Ryan Anderson
+
+string1 = 'Hellow, World!';
+switch = 1;
+if switch = 1
+string1
+    end
+
+    m = 3*4;
+
+%Let's try this again....
